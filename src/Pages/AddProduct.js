85feeae0464d39899e6a8aa45588ee6c331176ae
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-02-12 20:18:30 -0800

    Change to use props

--- src/Pages/AddProduct.js
+++ src/Pages/AddProduct.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const AddProduct = ({ products, setProducts }) => {
+const AddProduct = props => {
   const [productID, setProductId] = useState();
   const [sku, setSku] = useState();
   const [img, setImg] = useState();
@@ -14,25 +14,24 @@ const AddProduct = ({ products, setProducts }) => {
 
   function addProduct() {
     const newData = {
-      "id": productID,
-      "sku": sku,
-      "img": img,
-      "title": title,
-      "description": description,
-      "price": Number(price),
-      "quantity": Number(quantity),
-      "quantityInCart": 0,
-      "category": category
+      id: productID,
+      sku: sku,
+      img: img,
+      title: title,
+      description: description,
+      price: Number(price),
+      quantity: Number(quantity),
+      quantityInCart: 0,
+      category: category,
     };
-    products.push(newData);
-    console.log(`admin added product`, products);
+    props.products.push(newData);
   }
 
   return (
     <>
-      <Row className='mb-4'>
-        <Link to='/admin' className='mb-3'>
-          <Button className='sm' size='sm'>
+      <Row className="mb-4">
+        <Link to="/admin" className="mb-3">
+          <Button className="sm" size="sm">
             Return to inventory
           </Button>
         </Link>
@@ -41,68 +40,74 @@ const AddProduct = ({ products, setProducts }) => {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>ID</Form.Label>
-              <Form.Control 
-                type="text" 
-                className='login-form' 
-                onChange={e => setProductId(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setProductId(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>SKU</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setSku(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setSku(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Image</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setImg(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setImg(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Product Name</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setTitle(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setTitle(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Description</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setDescription(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setDescription(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Price</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setPrice(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setPrice(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Quantity</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setQuantity(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setQuantity(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Category</Form.Label>
-              <Form.Control 
-                type='text' 
-                className='login-form' 
-                onChange={e => setCategory(e.target.value)}/>
+              <Form.Control
+                type="text"
+                className="login-form"
+                onChange={e => setCategory(e.target.value)}
+              />
             </Form.Group>
-            <Button onClick={(addProduct)}>
-              Add product
-            </Button>
+            <Button onClick={addProduct}>Add product</Button>
           </Form>
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
 export default AddProduct;
